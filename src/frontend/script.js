288943e51e1382e29c92da097c0b7226bb5b8611
Daniel <danielkeenan75@yahoo.com>
2024-04-07 14:47:27 -0600

    Created Welcome page for application

---
+++ src/frontend/script.js
@@ -0,0 +1,8 @@
+// for now, the button doesn't execute any complex JavaScript. 
+// currently it shows how it could interact with JavaScript, by logging a message to the console when clicked
+
+
+document.getElementById('createAccount').addEventListener('click', function() {
+    console.log('Create Account button clicked');
+    // we can redirect the user to the account creation page or show the signup form
+});
