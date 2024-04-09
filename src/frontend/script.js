0dc13efe7193196dca08c63f48da18e3873bf2c9
madilynJ <pinkcandy704@gmail.com>
2024-04-08 20:36:04 -0400

    Added createaccount and sign in html files. Along with updated some of the style css and added more functionality onto the homepage to redirect the user correctly

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -4,5 +4,10 @@
 
 document.getElementById('createAccount').addEventListener('click', function() {
     console.log('Create Account button clicked');
-    // we can redirect the user to the account creation page or show the signup form
+    window.location.href = "createaccount.html"; //Directs user to the account creation page
+});
+
+document.getElementById('signIn').addEventListener('click', function() {
+    console.log('Sign In hyperlink was clicked');
+    window.location.href = "signin.html"; //Directs user to the sign in page
 });
